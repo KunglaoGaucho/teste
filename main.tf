c2907e190ee72ac2ee8822ceb12a55d4e9df216f
André Novaes <andrenovaes2210@gmail.com>
2025-11-05 16:40:10 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "meu_bucket" {
     Environment = "dev"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "access_good_1" {
+   bucket = aws_s3_bucket.meu_bucket.id
+
+   block_public_acls   = true
+   block_public_policy = true
+ }
